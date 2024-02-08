475c6eb14946e2aa557df8b27973677c9a34855d
Lucas Bach <lborgesbach@gmail.com>
2024-02-08 16:32:13 -0300

    adicionando php

---
+++ cad_aluno/php/pag.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {    
+    $nomeCompleto = $_POST['nomeCompleto'];
+    $idade = $_POST['idade'];
+    $dataNascimento = $_POST['dataNascimento'];
+    $telefone = $_POST['telefone'];
+    $endereco = $_POST['endereco'];
+    $numero = $_POST['numero'];
+    $plano = $_POST['plano'];
+}
+?>
+    
