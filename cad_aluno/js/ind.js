fac9c2e734ee4699436c1ef5119d849fb21557f0
Lucas Bach <lborgesbach@gmail.com>
2024-02-20 13:02:02 -0300

    data

---
+++ cad_aluno/js/ind.js
@@ -0,0 +1,9 @@
+document.getElementById('cep').addEventListener('blur', function() {
+    let cep = this.value.replace('-', '');
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('endereco').value = `${data.logradouro}, ${data.bairro}, ${data.localidade} - ${data.uf}`;
+        })
+        .catch(error => console.error('Erro ao buscar CEP:', error));
+}
