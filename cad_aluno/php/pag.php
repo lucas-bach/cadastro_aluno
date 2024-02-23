3dfd686e70836d1582f116907727b6ec309cbb5d
Lucas Bach <lborgesbach@gmail.com>
2024-02-23 10:22:20 -0300

    add.js/header

--- cad_aluno/php/pag.php
+++ cad_aluno/php/pag.php
@@ -1,10 +1,15 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {    
+
+
+header( 'Content-Type: application/json; charset=UTF-8' );
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {  
+    // Recupera os valores dos campos do formulário  
     $nomeCompleto = $_POST['nomeCompleto'];
     $idade = $_POST['idade'];
     $dataNascimento = $_POST['dataNascimento'];
     $telefone = $_POST['telefone'];
-    $endereco = $_POST['endereco'];
+    $endereco = utf8_decode($_POST['endereco']);
     $numero = $_POST['numero'];
     $plano = $_POST['plano'];
 }
